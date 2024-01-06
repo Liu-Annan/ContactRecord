a08c9983efe895e610c79a97b28009194e7ac560
Liu-Annan <3561007877@qq.com>
2024-01-06 20:07:28 +0800

    初始化项目

---
+++ Contact/test.c
@@ -0,0 +1,40 @@
+
+void menu()
+{
+	printf("*********************************\n");
+	printf("**** 1.add          2.delete ****\n");
+	printf("**** 2.search       4,modify ****\n");
+	printf("**** 5.display      6.sort   ****\n");
+	printf("**** 0.exit                  ****\n");
+	printf("*********************************\n");
+	printf("*********************************\n");
+}
+
+
+int main(void)
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("ÇëÊäÈë->\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case1:
+			break;
+		case2:
+			break;
+		case3:
+			break;
+		case4:
+			break;
+		case5:
+			break;
+		case6:
+			break;
+		}
+
+	} while (input);
+	
+}
